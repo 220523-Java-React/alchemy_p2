1f9eefa3f6738768525d17fd0207aca8830ac638
YourName <bilgamrinderpal@gmail.com>
2022-07-07 13:59:29 -0700

    Added properties on main

--- src/main/java/com/gamep2/game/GameApplication.java
+++ src/main/java/com/gamep2/game/GameApplication.java
@@ -2,6 +2,7 @@ package com.gamep2.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
 
 @SpringBootApplication
 public class GameApplication {
@@ -9,5 +10,7 @@ public class GameApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GameApplication.class, args);
 	}
+
+
 
 }
