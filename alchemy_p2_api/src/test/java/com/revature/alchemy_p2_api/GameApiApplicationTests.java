fa2d2d838d6e467e76b906a304b56a028c20dcfc
Haroun84 <mimih84@gmail.com>
2022-07-15 19:02:58 -0400

    Created new spring project

---
+++ alchemy_p2_api/src/test/java/com/revature/alchemy_p2_api/GameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.alchemy_p2_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
