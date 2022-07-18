504ebf53d2f93ab308b6e29d93c9ed3730b9326e
Efosa Ogbomo <efosaogbomoweb@gmail.com>
2022-07-18 09:02:44 -0700

    packages

---
+++ src/main/java/GameApplication.java
@@ -0,0 +1,20 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class GameApplication {
+
+    package com.gamep2.game;
+
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class GameApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(GameApplication.class, args);
+
+        }
+
+    }
+}
